95f43ab589f58d7ecc735e7e5b9556dc62104ea7
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-11-20 08:42:13 +1000

    Added personal inputs

--- src/components/Personal.vue
+++ src/components/Personal.vue
@@ -1,13 +1,25 @@
 <template>
   <v-layout column align-center>
     <h4 class="text-center">Personal</h4>
-    <v-text-field
-          name="input-1"
-          label="Name"
-          id="testing"
-          v-model="character.name"
-          @input="save"
-        ></v-text-field>
+    <v-text-field @input="save" label="Name" v-model="character.name"></v-text-field>
+    <v-text-field @input="save" label="Age" v-model="character.age" type="number" min="0"></v-text-field>
+    <v-text-field @input="save" label="Player" v-model="character.player"></v-text-field>
+    <v-text-field @input="save" label="Chronicle" v-model="character.chronicle"></v-text-field>
+    <v-text-field @input="save" label="Concept" v-model="character.concept"></v-text-field>
+
+    <v-text-field @input="save" label="Virtue" v-model="character.virtue"></v-text-field>
+    <v-text-field @input="save" label="Vice" v-model="character.vice"></v-text-field>
+    <v-text-field @input="save" label="Path" v-model="character.path"></v-text-field>
+    <v-text-field @input="save" label="Order" v-model="character.order"></v-text-field>
+    <v-text-field @input="save" label="Cabal" v-model="character.cabal"></v-text-field>
+
+    <v-text-field @input="save" label="Size" v-model="character.size" type="number" min="0"></v-text-field>
+    <v-text-field @input="save" label="Defense" v-model="character.defense" type="number" min="0"></v-text-field>
+    <v-text-field @input="save" label="Initiative" v-model="character.initiative" type="number" min="0"></v-text-field>
+    <v-text-field @input="save" label="Speed" v-model="character.speed" type="number" min="0"></v-text-field>
+    <v-text-field @input="save" label="Experience" v-model="character.experience" type="number"></v-text-field>
+    <v-text-field @input="save" label="Cabal xp" v-model="character.cabalxp" type="number"></v-text-field>
+    <v-text-field @input="save" label="Arcane xp" v-model="character.arcanexp" type="number"></v-text-field>
   </v-layout>
 </template>
 
