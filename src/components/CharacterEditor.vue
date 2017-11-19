580778599a0d253dd064a8b966381687c2a114b6
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-11-20 00:47:22 +1000

    Details from personal tab updating

--- src/components/CharacterEditor.vue
+++ src/components/CharacterEditor.vue
@@ -1,50 +1,62 @@
 <template>
   <v-layout column align-center>
-    <h4 class="text-center">Personal</h4>
-    <v-text-field
-          name="input-1"
-          label="Name"
-          id="testing"
-          v-model="character.name"
-          @input="save"
-        ></v-text-field>
+  <v-tabs v-model="activeTab">
+      <v-tabs-bar class="black" dark>
+        <v-tabs-item
+          v-for="tab in tabs"
+          :key="tab.name"
+          :href="'#' + tab.name"
+          ripple
+        >
+        {{ tab.name }}
+        </v-tabs-item>
+        <v-tabs-slider color="red"></v-tabs-slider>
+      </v-tabs-bar>
+      <v-tabs-items>
+        <v-tabs-content
+          v-for="tab in tabs"
+          :key="tab.name"
+          :id="tab.name"
+        >
+          <v-card flat>
+            <component :is="tab.componentType" v-model="character"></component>
+          </v-card>
+        </v-tabs-content>
+      </v-tabs-items>
+    </v-tabs>
   </v-layout>
 </template>
 
 <script>
-import CharacterDao from '../dataAccess/CharacterDao.js'
-import math from '../dataAccess/math.js'
+import CharacterDao from "../dataAccess/CharacterDao.js";
+import Personal from "./Personal.vue";
 
-
-var getLastSavedCharacter = function(){
+var getLastSavedCharacter = function() {
   var characters = CharacterDao.getLocalCharacterIds();
-  var characterId = characters[characters.length-1];
-  if(!characterId) return {"name":"Unamed Character"};
+  var characterId = characters[characters.length - 1];
+  if (!characterId) return { name: "Unamed Character" };
 
   return CharacterDao.loadLocally(characterId);
 };
 
 export default {
-  data () {
+  data() {
     return {
       message: "This is the Character Editor!",
       character: getLastSavedCharacter(),
       characterDao: CharacterDao,
-    }
+      tabs: [{ name: "Personal", componentType: "Personal" }],
+      activeTab: null
+    };
   },
-  methods:{
-      save: function(){
-        this.$eventHub.$emit('saveCharacterToFile');
-      }
-  },
-  created: function(){
+  created: function() {
     console.log("Character Editor Created");
-    this.$eventHub.$on('saveCharacterToFile', ()=>{ 
-      console.log("saveCharacterToFile");
+    this.$eventHub.$on("saveCharacterToFile", () => {
       this.characterDao.saveLocally(this.character);
     });
   },
   components: {
+    Personal: Personal
   }
-}
+};
 </script>
---
+++ src/components/Personal.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout column align-center>
+    <h4 class="text-center">Personal</h4>
+    <v-text-field
+          name="input-1"
+          label="Name"
+          id="testing"
+          v-model="character.name"
+          @input="save"
+        ></v-text-field>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: "character"
+  },
+  methods: {
+    save: function() {
+      this.$eventHub.$emit("saveCharacterToFile");
+    }
+  },
+  props: ["character"],
+  data() {
+    return {};
+  }
+};
+</script>
