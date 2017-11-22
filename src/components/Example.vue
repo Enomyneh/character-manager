6d9c5d7ec013b4579ad3eb169413cb504b83cbfc
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-11-23 00:59:23 +1000

    Updated example section

---
+++ src/components/Example.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container grid-list-md text-xs-center>
+              <h4 class="text-center">Section</h4>
+        <Dots :min="1" :max="10" v-model="character.gnosis"
+            @increment="incrementValue()"
+            @decrement="decrementValue()"
+            ></Dots>
+  </v-container>
+</template>
+
+<script>
+import Dots from "./input/Dots.vue";
+
+export default {
+  model: {
+    prop: "character"
+  },
+  props: ["character"],
+  data() {
+    return {};
+  },
+  methods: {
+    unusedSpace: function() {
+      return 10;
+    },
+    incrementValue: function() {
+      this.$eventHub.$emit("saveCharacterToFile");
+    },
+    decrementValue: function() {
+      this.$eventHub.$emit("saveCharacterToFile");
+    }
+  },
+  components: {
+    Dots: Dots
+  }
+};
+</script>
