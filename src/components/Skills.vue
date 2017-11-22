1fafa7a3a430877fc57d7a89de3fbd50e2e6b3cd
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-11-22 12:54:48 +1000

    Made specialties togglable.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,20 @@
 <template>
   <v-container grid-list-md text-xs-center>
+        <v-btn small color="primary" dark @click="toggleAllSpecialties()">Specs</v-btn>
     <h4 class="text-center">Skills</h4>
     <v-layout row wrap>
     <v-flex xs12 sm4 v-for="category in categories" :key="category">
       <h5 class="text-center text-muted ">{{category}}</h5>
       <v-layout row wrap v-for="skill in getSkillsByCategory(category)" :key="skill.name ">
+        <!-- <v-flex xs2>
+                <v-btn small color="primary" dark @click="toggleSpecialties(skill.name)">Specs</v-btn>
+
+        </v-flex> -->
         <v-flex xs6>
+            <!-- <v-badge left>
+                <span slot="badge" @click="toggleSpecialties(skill.name)">S</span>
+                <span>{{skill.name}}</span>
+            </v-badge> -->
             <h4 class="text-center">{{skill.name}}</h4>
         </v-flex>
         <v-flex xs6>
@@ -15,6 +24,7 @@
                 ></Dots>
         </v-flex>
         <v-text-field 
+          v-if="showSpecialities(skill.name)"
           label="Specialties" 
           v-model="character.specialties[skill.name.toLowerCase()]" 
           @input="save"></v-text-field>          
@@ -46,12 +56,32 @@ export default {
     decrementValue: function(fieldName) {
       this.character.decrementValue(fieldName);
       this.save();
+    },
+    showSpecialities: function(skillName) {
+      return (
+        this.showAllSpecialities || this.specialtiesShown.includes(skillName)
+      );
+    },
+    toggleSpecialties: function(skillName) {
+      if (this.specialtiesShown.includes(skillName)) {
+        this.specialtiesShown.splice(
+          this.specialtiesShown.indexOf(skillName),
+          1
+        );
+      } else {
+        this.specialtiesShown.push(skillName);
+      }
+    },
+    toggleAllSpecialties: function() {
+      this.showAllSpecialities = !this.showAllSpecialities;
     }
   },
   props: ["character"],
   data() {
     return {
-      categories: ["Mental", "Physical", "Social"]
+      categories: ["Mental", "Physical", "Social"],
+      specialtiesShown: [],
+      showAllSpecialities: false
     };
   },
   components: {
